f73b56a8ffb23292cf69387d02f2f331d22431e3
Renato Franco <renatofr95@gmail.com>
2021-01-28 16:36:57 -0300

    Inserção de usuário com POST

--- src/main/java/com/renatofranco/workshopmongo/services/UserService.java
+++ src/main/java/com/renatofranco/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.renatofranco.workshopmongo.services;
 
 import com.renatofranco.workshopmongo.domain.User;
+import com.renatofranco.workshopmongo.dto.UserDTO;
 import com.renatofranco.workshopmongo.repository.UserRepository;
 import com.renatofranco.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Usuario não encontrado"));
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 }
